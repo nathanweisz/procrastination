c48e4ab8f49d419baa60dc424a2ab153eecb5dd7
Your Name <b1019548@nathan-weiszs-computer.local>
2020-01-18 12:43:23 +0100

    updated snipp_test_tools. alignment on Grand Average

--- snipps_test_tools.m
+++ snipps_test_tools.m
@@ -10,13 +10,30 @@ addpath('~/Documents/MATLAB/meeg_hyperalignment/')
 load('/Users/b1019548/Desktop/MessingAround/Markov_Orig_ERFs/AllERFs.mat')
 
 %%
+
+cfg=[];
+cfg.baseline=[-.05 0];
+for ii=1:length(OMmat_RD)
+    SNDmat{ii}=ft_timelockbaseline(cfg, SNDmat{ii});
+    OMmat_RD{ii}=ft_timelockbaseline(cfg, OMmat_RD{ii});
+    OMmat_MM{ii}=ft_timelockbaseline(cfg, OMmat_MM{ii});
+    OMmat_MP{ii}=ft_timelockbaseline(cfg, OMmat_MP{ii});
+    OMmat_OR{ii}=ft_timelockbaseline(cfg, OMmat_OR{ii});
+end 
+
+%%
 cfg=[];
 cfg.lpfilter='yes';
 cfg.lpfreq=25;
     
 for ii=1:length(SNDmat)
     SNDmat{ii}=ft_preprocessing(cfg, SNDmat{ii});
+    OMmat_MM{ii}=ft_preprocessing(cfg, OMmat_MM{ii});
+    OMmat_MP{ii}=ft_preprocessing(cfg, OMmat_MP{ii});
+    OMmat_OR{ii}=ft_preprocessing(cfg, OMmat_OR{ii});
+    OMmat_RD{ii}=ft_preprocessing(cfg, OMmat_RD{ii});
 end
+
 
 %%
 cfg=[];
@@ -95,11 +112,12 @@ cfg.inverse=true;
 SNDsub2_inv=nw_procrustes_applytr(cfg, SNDmat_p{2});
 
 %%
+SNDsub2_inv.avg=SNDsub2_inv.avg*1.1; % scale up to make lines visible
+
 cfg=[];
 cfg.xlim=[-.1 .3];
 cfg.layout='neuromag306mag.lay';
-ft_multiplotER(cfg, SNDsub2_inv)
-figure;ft_multiplotER(cfg, SNDmat{2})
+ft_multiplotER(cfg, SNDmat{2}, SNDsub2_inv)
 
 %%
 cfg=[];
@@ -117,7 +135,6 @@ cfg.template    = 'neuromag306mag_neighb.mat';                % specify type of 
 cfg.layout      = 'neuromag306mag_helmet.mat';                % specify layout of channels
 cfg.feedback    = 'yes';                              % show a neighbour plot
 neighbours      = ft_prepare_neighbours(cfg, GA_OMmm_p); %
-
 
 %%
 cfg=[];
@@ -144,12 +161,12 @@ cfg.parameter='stat';
 cfg.xlim=[0 .3];
 cfg.layout='neuromag306mag_helmet.lay';
 cfg.zlim='absmax';
-ft_multiplotER(cfg, stat, statP) % tatP.posclusters(1) --> prob: 0.0020
+ft_multiplotER(cfg, stat, statP) % statP.negclusters(1) --> prob: 0.020 .. statP.posclusters(1) --> prob: 0.040
 
 %%
 
-statP.stat2=statP.stat.*(statP.posclusterslabelmat==1);
-stat.stat2=stat.stat.*(statP.posclusterslabelmat==1);
+statP.stat2=statP.stat.*(statP.negclusterslabelmat==1) + statP.stat.*(statP.posclusterslabelmat==1);
+stat.stat2=stat.stat.*(statP.negclusterslabelmat==1) + stat.stat.*(statP.posclusterslabelmat==1);
 
 cfg=[];
 cfg.parameter='stat2';
@@ -196,4 +213,134 @@ cfg.zlim='absmax';
 ft_multiplotER(cfg, test) 
 figure; ft_multiplotER(cfg, test_P_back) 
 
+%%%%%%%%%%%%%%%%%%%
+%% omission responses in space of subj 1 ... let's tranform to subj 27 and redo stat
 
+OMmat_RD_p2 = OMmat_RD_p;
+OMmat_MM_p2 = OMmat_MM_p;
+OMmat_MP_p2 = OMmat_MP_p;
+OMmat_OR_p2 = OMmat_OR_p;
+
+for ii=1:length(OMmat_RD_p)
+    cfg=[];
+    cfg.tr=tr{27};
+    cfg.inverse=true;
+    OMmat_RD_p2{ii}=nw_procrustes_applytr(cfg, OMmat_RD_p{ii});
+    OMmat_MM_p2{ii}=nw_procrustes_applytr(cfg, OMmat_MM_p{ii});
+    OMmat_MP_p2{ii}=nw_procrustes_applytr(cfg, OMmat_MP_p{ii});
+    OMmat_OR_p2{ii}=nw_procrustes_applytr(cfg, OMmat_OR_p{ii});
+end
+
+%%
+cfg=[];
+cfg.method      = 'montecarlo';
+cfg.statistic   = 'ft_statfun_depsamplesT';
+cfg.correctm    = 'cluster';
+cfg.correcttail = 'prob';
+cfg.latency=[0 .3];
+cfg.numrandomization = 1000;
+cfg.neighbours  = neighbours;
+cfg.spmversion = 'spm12';
+Nsub = length(SNDmat);
+cfg.design(1,1:2*Nsub)  = [ones(1,Nsub) 2*ones(1,Nsub)];
+cfg.design(2,1:2*Nsub)  = [1:Nsub 1:Nsub];
+cfg.ivar                = 1; % the 1st row in cfg.design contains the independent variable
+cfg.uvar                = 2;
+
+statP2=ft_timelockstatistics(cfg, OMmat_RD_p2{:}, OMmat_OR_p2{:});
+
+%%
+cfg=[];
+cfg.parameter='stat';
+cfg.xlim=[0 .3];
+cfg.layout='neuromag306mag_helmet.lay';
+cfg.zlim='absmax';
+ft_multiplotER(cfg, statP, statP2) % NO CLUSTER!
+
+%%%%%% 
+%% REDO ON GA
+
+cfg=[];
+cfg.scale=false;
+
+for ii=1:length(SNDmat)
+    tmp=nw_procrustes_calctr(cfg, GA_SND, SNDmat{ii});
+    SNDmat_pGA{ii}=tmp.dataP;
+    trGA{ii}=tmp.tr;
+    clear tmp
+end
+
+
+%%
+GA_SND_pGA = ft_timelockgrandaverage([], SNDmat_pGA{:});
+
+%%
+cfg=[];
+cfg.layout='neuromag306mag.lay';
+ft_multiplotER(cfg, GA_SND_pGA ,GA_SND_p, GA_SND)
+
+%%
+
+OMmat_RD_pGA = OMmat_RD;
+OMmat_MM_pGA = OMmat_MM;
+OMmat_MP_pGA = OMmat_MP;
+OMmat_OR_pGA = OMmat_OR;
+
+for ii=1:length(OMmat_RD)
+    cfg=[];
+    cfg.tr=trGA{ii};
+    OMmat_RD_pGA{ii}=nw_procrustes_applytr(cfg, OMmat_RD{ii});
+    OMmat_MM_pGA{ii}=nw_procrustes_applytr(cfg, OMmat_MM{ii});
+    OMmat_MP_pGA{ii}=nw_procrustes_applytr(cfg, OMmat_MP{ii});
+    OMmat_OR_pGA{ii}=nw_procrustes_applytr(cfg, OMmat_OR{ii});
+end
+
+%%
+
+GA_OMrd_pGA = ft_timelockgrandaverage([], OMmat_RD_pGA{:});
+GA_OMmm_pGA = ft_timelockgrandaverage([], OMmat_MM_pGA{:});
+GA_OMmp_pGA = ft_timelockgrandaverage([], OMmat_MP_pGA{:});
+GA_OMor_pGA = ft_timelockgrandaverage([], OMmat_OR_pGA{:});
+
+%%
+cfg=[];
+cfg.xlim=[-.1 .3];
+cfg.layout='neuromag306mag.lay';
+ft_multiplotER(cfg, GA_OMrd_pGA, GA_OMmm_pGA, GA_OMmp_pGA ,GA_OMor_pGA)
+
+
+%%
+cfg=[];
+cfg.method      = 'montecarlo';
+cfg.statistic   = 'ft_statfun_depsamplesT';
+cfg.correctm    = 'cluster';
+cfg.correcttail = 'prob';
+cfg.latency=[0 .3];
+cfg.numrandomization = 1000;
+cfg.neighbours  = neighbours;
+cfg.spmversion = 'spm12';
+Nsub = length(SNDmat);
+cfg.design(1,1:2*Nsub)  = [ones(1,Nsub) 2*ones(1,Nsub)];
+cfg.design(2,1:2*Nsub)  = [1:Nsub 1:Nsub];
+cfg.ivar                = 1; % the 1st row in cfg.design contains the independent variable
+cfg.uvar                = 2;
+
+statPGA=ft_timelockstatistics(cfg, OMmat_RD_pGA{:}, OMmat_OR_pGA{:});
+
+%%
+cfg=[];
+cfg.parameter='stat';
+cfg.layout='neuromag306mag_helmet.lay';
+cfg.zlim='absmax';
+ft_multiplotER(cfg, statPGA) % statPGA.negclusters(1) --> prob: 0.0020,statPGA.negclusters(2) --> prob: 0.024 
+
+%%
+
+statPGA.stat2=statPGA.stat.*(statPGA.negclusterslabelmat==1) +statPGA.stat.*(statPGA.negclusterslabelmat==2);
+
+cfg=[];
+cfg.parameter='stat2';
+cfg.xlim=[0 .3];
+cfg.layout='neuromag306mag_helmet.lay';
+cfg.zlim='absmax';
+ft_multiplotER(cfg, statPGA) 
